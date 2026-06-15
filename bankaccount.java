0776b82bef9d3f83ac228c1429c5999e9b0abd91
Bisma Riaz <rbisma616@gmail.com>
2026-06-15 09:33:38 +0500

    Delete BankAccount class and its methods
    Removed the BankAccount class and its methods.

--- bankaccount.java
+++ bankaccount.java
@@ -1,23 +1,4 @@
-class BankAccount {
-    double balance;
 
-    BankAccount(double balance) {
-        this.balance = balance;
-    }
-
-    void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposited: " + amount);
-    }
-
-    void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrawn: " + amount);
-        } else {
-            System.out.println("Insufficient Balance");
-        }
-    }
 
     void showBalance() {
         System.out.println("Current Balance: " + balance);
